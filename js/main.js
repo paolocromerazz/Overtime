f035adf7579df69bb0b13480ba1986463ef2799a
paolocromerazz <paolocromerazz@gmail.com>
2018-03-19 13:04:07 +0100

    first attempt avoiding cross origin error

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// something here
+let overtimeRate=0;
+let hoursWorked=0;
+
+window.onload = function() {
+    var a = document.getElementById("load");
+    a.onclick = readTextFile();
+  }
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+//usage:
+readTextFile("fullstackPeople.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+});
