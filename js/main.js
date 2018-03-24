15c0040674a7a88932b66c0d9443b0285a63ca1c
paolocromerazz <paolocromerazz@gmail.com>
2018-03-25 00:15:29 +0100

    20180315

--- js/main.js
+++ js/main.js
@@ -1,32 +1,62 @@
-// something here
-let overtimeRate=0;
-let hoursWorked=0;
-
-window.onload = function() {
-    
-    var a = document.getElementById("load");
-    a.onclick = readTextFile("../fullstackPeople.json", function(text){
-        alert("ciao");
-        var data = JSON.parse(text);
-        alert(data);
-        console.log(data);
-    });
-  }
-
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    //rawFile.send(null);
+function a() {
+    document.getElementById('files').addEventListener('change', handleFileSelect, false);
 }
 
-//usage:
-// readTextFile("fullstackPeople.json", function(text){
-//     var data = JSON.parse(text);
-//     console.log(data);
-// });
+
+
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    for (var i = 0, f; f = files[i]; i++) {
+        // json only
+        if (!f.type.match('json.*')) {
+            continue;
+        }
+
+        var reader = new FileReader();
+        reader.onload = (function (theFile) {
+            return function (e) {
+                //console.log(e.target.result);
+                var struct = JSON.parse(e.target.result);
+                var txt = "<table class='tab' border='0'>";
+                //console.log(struct.Rules[0].payRate);
+                var due = struct.Rules[0].due;
+                var done;
+                var payRate = struct.Rules[0].payRate;
+                var overtimeRate = struct.Rules[0].overtimeRate;
+
+                for (j in struct.Employees) {
+                    done = struct.Employees[j].hoursWorked;
+                    txt += "<tr>" +
+                        "<td>  " + struct.Employees[j].userId + "  </td>" +
+                        "<td>  " + struct.Employees[j].comment + "  </td>" +
+                        "<td>  " + struct.Employees[j].hoursWorked + " hours worked  </td>" +
+                        "<td>  The employee " + overtimeText(due,done) + " made " + calc(due, done, payRate, overtimeRate) + " $ this week   </td>" +
+                        "</tr>";
+                }
+                txt += "</table>";
+                document.getElementById("tb").innerHTML = txt;
+            };
+        })(f);
+        // Read in the file as a data URL.
+        //reader.readAsDataURL(f);
+        reader.readAsText(f);
+    }
+}
+
+function calc(due, done, payRate, overtimeRate) {
+    var overtime = done - due;
+    var overtimeCond = (overtime > 0);
+    var toPay = done * payRate;
+    if (overtimeCond) {
+        toPay += overtime * overtimeRate;
+    }
+    return toPay;
+}
+
+function overtimeText(due, done) {
+    if ((done - due) > 0)
+        return "worked overtime and";
+    else
+        return "";
+}
